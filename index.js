52c7606c4110dd85b8c318ae5664a74c8fabbccf
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-02-10 10:30:32 +0530

    assignment 2

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const express = require('express');
 const { resolve } = require('path');
+const mongoose = require('mongoose');
+const User = require('./schema.js');
+const env = require('dotenv').config();
+
+
 
 const app = express();
 const port = 3010;
 
 app.use(express.static('static'));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});
+
+app.post('/api/users', async (req, res) => {
+  try {
+    const userData = req.body;
+
+    const newUser = new User(userData);
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res
+        .status(400)
+        .json({ message: 'Validation error', details: error.message });
+    } else {
+      res.status(500).json({ message: 'Server error', details: error.message });
+    }
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
