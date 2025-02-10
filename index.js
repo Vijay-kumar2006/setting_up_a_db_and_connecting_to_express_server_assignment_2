2312a3c0a796e8b7cfcdd04c4a413ad4fc45361b
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-02-10 11:43:15 +0530

    2

--- index.js
+++ index.js
@@ -3,49 +3,38 @@ const { resolve } = require('path');
 const mongoose = require('mongoose');
 const User = require('./schema.js');
 const env = require('dotenv').config();
+const connectDB = require('./db.js');
 
 
 
 const app = express();
-const port = 3010;
+
 
 app.use(express.static('static'));
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB', err);
-});
-
-app.post('/api/users', async (req, res) => {
-  try {
-    const userData = req.body;
-
-    const newUser = new User(userData);
-    await newUser.save();
-
-    res.status(201).json({ message: 'User created successfully' });
-  } catch (error) {
-    if (error.name === 'ValidationError') {
-      res
-        .status(400)
-        .json({ message: 'Validation error', details: error.message });
-    } else {
-      res.status(500).json({ message: 'Server error', details: error.message });
-    }
-  }
-});
-
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+const port = 3100;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
+});
+
+
+app.post('/add', async (req, res) => {
+  try{
+    const usedData = req.body;
+    const newUser = new User(usedData);
+    await newUser.save();
+    res.status(200).json('User added successfully');
+
+  }catch(error){
+    if(error =='ValidationError'){
+      res.status(400).json({ message: 'Validation error', details: error.message });
+  }
+  else{
+    res.status(500).json({ message: 'Server error', details: error.message });
+  }
+  }
 });
